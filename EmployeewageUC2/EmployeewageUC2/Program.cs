15348070db1a95b64fd60ca643a5205eaa616864
naveen962 <mrn28897@gmail.com>
2021-09-24 04:01:12 -0700

    Checking whether the Employee is present or absent program is added

---
+++ EmployeewageUC2/EmployeewageUC2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random object created
+            Random ran = new Random();
+            int EmpCheck = ran.Next(0, 2);
+            int Fulltime = 1;
+            //condition
+            if (EmpCheck == Fulltime)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+    }
+}
