c1e22d8eecf41142bf0fd23147544b26f3f2e043
naveen962 <mrn28897@gmail.com>
2021-09-24 04:10:25 -0700

    EmployeeWage problem using switch case statement is added

---
+++ EmployeeWageUC4/EmployeeWageUC4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWages
+{
+    class UseCase4
+    {
+        public static void AddPartimeEmp()
+
+        {
+            //constants
+            int EMPWAGE_PER_HOUR = 20;
+            int Num_Working_of_days = 20;
+
+
+            //variables
+            int EmpHours = 0;
+            int Empwage = 0;
+
+
+            //computation
+            Random ran = new Random();
+            int Empcheck = ran.Next(0, 3);
+            switch (Empcheck)
+            {
+                case 1:
+                    Console.WriteLine("PART TIME");
+                    EmpHours = 4;
+                    break;
+
+                case 2:
+                    Console.WriteLine("FULL Time");
+                    EmpHours = 8;
+                    break;
+                default:
+                    Console.WriteLine("ABSENT");
+                    EmpHours = 0;
+                    break;
+
+            }
+
+            Empwage = EmpHours * EMPWAGE_PER_HOUR;
+            Console.WriteLine("EMPLOYEE WAGE =" + Empwage);
+
+        }
+    }
+}
